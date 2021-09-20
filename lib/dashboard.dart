c46583207a0f4e095fcd0fb658c805795a1ee12d
codingcafe1 <alizeb875@gmail.com>
2021-09-20 12:54:26 +0500

    initial commit

---
+++ lib/dashboard.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+
+class DashBoard extends StatefulWidget {
+  const DashBoard({Key? key}) : super(key: key);
+
+  @override
+  _DashBoardState createState() => _DashBoardState();
+}
+
+
+
+class _DashBoardState extends State<DashBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/logout.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+
+
+class Logout extends StatefulWidget {
+  const Logout({Key? key}) : super(key: key);
+
+  @override
+  _LogoutState createState() => _LogoutState();
+}
+
+
+class _LogoutState extends State<Logout> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
